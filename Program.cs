1586d1786539ce885fde2da10dcea9ded3f1b94f
Ahmet Furkan ÖZDEMİR <ozdemirahmetfurkan19@gmail.com>
2026-05-18 20:20:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-public void BolmeIslemi(int sayi1, int sayi2) { 
-    int sonuc = sayi1 / sayi2; 
-    Console.WriteLine("Cevap: " + sonuc); 
-//test
-    //test
-    //tes
-    //testtt
-    // test
-    //test2
-}
+using System;
 
+namespace CodeReviewTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yapay Zeka Testi Başlıyor...");
+            
+            int[] sayilar = { 10, 20, 30 };
+            
+            // Bilerek yapılan hata: Dizide sadece 3 eleman var (indeks 0, 1, 2)
+            // Ama biz 5. indeksi çağırmaya çalışıyoruz.
+            int hataliSayi = sayilar[5]; 
+            
+            Console.WriteLine("Sayi: " + hataliSayi);
+        }
+    }
+}
